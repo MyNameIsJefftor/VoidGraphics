db68695662a0d7f9b1730d3e08af590a58a99419
Kurtis McCammon <maxgamer898@gmail.com>
2020-10-01 19:51:56 -0400

    Added Base code for creating a triangle and correctly setup GLFW

---
+++ VoidEngine/Main.cpp
@@ -0,0 +1,159 @@
+#include <iostream>
+
+#include <glad/glad.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* windo, int width, int height);
+void processInput(GLFWwindow* window);
+void renderTriangle();
+void createVertexShader();
+void createFragShader();
+
+const char* vertexShaderSource = "#version 330 core\n"
+	"layout (location = 0) in vec3 aPos;\n"
+	"void main()\n"
+	"{\n"
+	"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"}\0";
+
+const char* FragColorSource = "#version 330 core\n"
+	"out vec4 FragColor;\n"
+	"void main()\n"
+	"{\n"
+	"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"}\n";
+
+
+float vertices[] = {
+	-0.5f,-0.5f,0.0f,
+	0.5f,-0.5f, 0.0f,
+	0.0f,0.5f,0.0f
+};
+
+int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "++Void", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failure to create window." << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failure to load GLAD(os)" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	//ShaderTime
+
+	//VertexShader
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER); //tell GL that this is a vertexShader
+
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);//Set the shader to the one we wrote.
+	glCompileShader(vertexShader); //Compile the Shader code
+
+	//FragShader/ColorShader
+	unsigned int fragShader;
+	fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	glShaderSource(fragShader, 1, &FragColorSource, NULL);
+	glCompileShader(fragShader);
+
+	//Create the program
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);//attach vertex and kermit de frag.
+	glAttachShader(shaderProgram, fragShader);
+	glLinkProgram(shaderProgram); //Link
+
+
+	glUseProgram(shaderProgram); //Tell gl to use our new shader
+
+	glDeleteShader(vertexShader); //Don't need these anymore. The program has them now.
+	glDeleteShader(fragShader);
+
+	//Create Triangle
+	unsigned int VBO; //Vertex Buffer Object
+	glGenBuffers(1, &VBO); //Give the object an id
+
+	unsigned int VAO; //Vertex Array object
+	glGenVertexArrays(1, &VAO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//Tell gl how to read our vertex
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+
+
+	while (!glfwWindowShouldClose(window)) {
+		//Get input
+		processInput(window);
+
+		//Perform WRENDER
+		glClearColor(0.1f, 0.2f, 0.2f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		//Swap the buffs. Check for events.
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* windo, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+void renderTriangle() {
+	unsigned int VBO; //Vertex Buffer Object
+	glGenBuffers(1, &VBO); //Give the object an id
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);//bind VBO to an array buffer
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+}
+
+
+void createVertexShader() {
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER); //tell GL that this is a vertexShader
+
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);//Set the shader to the one we wrote.
+	glCompileShader(vertexShader); //Compile the Shader code
+
+}
+
+void createFragShader() {
+	unsigned int fragShader;
+	fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	glShaderSource(fragShader, 1, &FragColorSource, NULL);
+	glCompileShader(fragShader);
+}
