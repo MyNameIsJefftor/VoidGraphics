e447d7250d20919d1433a1c911c52961e4ddcf4e
Kurtis McCammon <maxgamer898@gmail.com>
2020-10-02 15:20:23 -0400

    Added GLM (OpenGL Math) and a wrote a tool for reading files and turning them into shaders.(ShaderReader.h)

--- VoidEngine/Main.cpp
+++ VoidEngine/Main.cpp
@@ -3,6 +3,9 @@
 #include <glad/glad.h>
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
+#include <glm/common.hpp>
+#include "ShaderReader.h"
+
 
 void framebuffer_size_callback(GLFWwindow* windo, int width, int height);
 void processInput(GLFWwindow* window);
@@ -25,10 +28,16 @@ const char* FragColorSource = "#version 330 core\n"
 	"}\n";
 
 
-float vertices[] = {
-	-0.5f,-0.5f,0.0f,
-	0.5f,-0.5f, 0.0f,
-	0.0f,0.5f,0.0f
+float tVertices[] = {
+	 0.5f,  0.5f, 0.0f,  // top right
+	 0.5f, -0.5f, 0.0f,  // bottom right
+	-0.5f, -0.5f, 0.0f,  // bottom left
+	-0.5f,  0.5f, 0.0f   // top left 
+};
+
+unsigned int tIndex[] = {  // note that we start from 0!
+	0, 1, 3,   // first triangle
+	1, 2, 3    // second triangle
 };
 
 int main() {
@@ -91,10 +100,16 @@ int main() {
 	unsigned int VAO; //Vertex Array object
 	glGenVertexArrays(1, &VAO);
 
+	unsigned int EBO;//Element buffer object. INDEX
+	glGenBuffers(1, &EBO);
+
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(tVertices), tVertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(tIndex), tIndex, GL_STATIC_DRAW);
 
 	//Tell gl how to read our vertex
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -112,7 +127,8 @@ int main() {
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 
 		//Swap the buffs. Check for events.
 		glfwSwapBuffers(window);
@@ -137,7 +153,7 @@ void renderTriangle() {
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);//bind VBO to an array buffer
 
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(tVertices), tVertices, GL_STATIC_DRAW);
 }
 
 
---
+++ VoidEngine/ShaderReader.h
@@ -0,0 +1,113 @@
+#pragma once
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class Shader
+{
+public:
+	//Program ID
+	unsigned int ID;
+	Shader(const char* vertexPath, const char* fragPath);
+
+	void use();
+
+	void setBool(const std::string& name, bool value) const;
+	void setInt(const std::string& name, int value) const;
+	void setFloat(const std::string& name, float value) const;
+};
+
+Shader::Shader(const char* vertexPath, const char* fragmentPath)
+{
+    //retrieve the vertex/fragment source code from filePath
+    std::string vertexCode;
+    std::string fragmentCode;
+    std::ifstream vShaderFile;
+    std::ifstream fShaderFile;
+    // ensure ifstream objects can throw exceptions:
+    vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    try
+    {
+        // open
+        vShaderFile.open(vertexPath);
+        fShaderFile.open(fragmentPath);
+        std::stringstream vShaderStream, fShaderStream;
+        // read file into streams
+        vShaderStream << vShaderFile.rdbuf();
+        fShaderStream << fShaderFile.rdbuf();
+        // close file
+        vShaderFile.close();
+        fShaderFile.close();
+        // convert stream -> string
+        vertexCode = vShaderStream.str();
+        fragmentCode = fShaderStream.str();
+    }
+    catch (std::ifstream::failure e)
+    {
+        std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+    }
+    const char* vShaderCode = vertexCode.c_str();
+    const char* fShaderCode = fragmentCode.c_str();
+
+    unsigned int vertex, frag;
+    int success;
+    char infoLog[512];
+
+    vertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex, 1, &vShaderCode, NULL);
+    glCompileShader(vertex);
+
+    glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertex, 512, NULL, infoLog);
+        std::cout << "ERR : Shader < Vertex < CompFail\n" << infoLog << std::endl;
+    };
+
+    frag = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(frag, 1, &fShaderCode, NULL);
+    glCompileShader(frag);
+
+    glGetShaderiv(frag, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(frag, 512, NULL, infoLog);
+        std::cout << "ERR : Shader < Frag < CompFail\n" << infoLog << std::endl;
+    };
+    
+    ID = glCreateProgram();
+    glAttachShader(ID, vertex);
+    glAttachShader(ID, frag);
+    glLinkProgram(ID);
+
+    glGetProgramiv(ID, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(ID, 512, NULL, infoLog);
+        std::cout << "ERR : Shader < Program < LinkFail\n" << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertex);
+    glDeleteShader(frag);
+};
+
+void Shader::use() {
+    glUseProgram(ID);
+}
+
+void Shader::setBool(const std::string& name, bool value) const {
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+}
+void Shader::setInt(const std::string& name, int value) const {
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+}
+void Shader::setFloat(const std::string& name, float value) const {
+    glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+
+}
+#endif
+
